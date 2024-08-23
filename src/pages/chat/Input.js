ba6c38c75f69e0c016aeec89445674f1e57cab7a
Diego <diegocdsantos@gmail.com>
2024-08-22 22:38:53 -0300

    added the input component

--- src/pages/chat/Input.js
+++ src/pages/chat/Input.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useState } from "react";
+import InputMask from "react-input-mask";
 
 export default function Input() {
+  const [phone, setPhone] = useState("");
+  const [isPhoneValid, setIsPhoneValid] = useState(false);
+  const [error, setError] = useState("");
+
+  const handleClick = (e) => {
+    console.log(phone);
+  };
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    if (/^\d*$/.test(value) && value.length <= 11) {
+      setPhone(value);
+      setIsPhoneValid(value.length === 11);
+    }
+  };
+
   return (
-    <div className='justify-self-end p-2 lg:p-4 max-w-[80%] lg:max-w-[60%] rounded-md bg-gray-300'>
-        <p>Input</p> 
+    <div className="justify-self-end flex px-4 py-2 lg:p-4 gap-4 max-w-[80%] lg:max-w-[60%] rounded-md bg-white">
+
+      <input
+        className="border-b-2 focus:outline-none w-[70%]"
+        type="text"
+        placeholder="## ##### ####"
+        value={phone}
+        onChange={handleChange}
+      ></input>
+
+      {isPhoneValid && (
+        <button className="bg-teal-600 rounded-xl active:scale-90 shadow-md shadow-gray-700" onClick={handleClick}>
+          <p className="text-white py-1 px-2">Enviar</p>
+        </button>
+      )}
+
+      {!isPhoneValid && (
+        <button
+          disabled
+          className="bg-gray-300 rounded-xl"
+          onClick={handleClick}
+        >
+          <p className="text-white py-1 px-2">Enviar</p>
+        </button>
+      )}
     </div>
-  )
+  );
 }
--- src/pages/chat/MessageBox.js
+++ src/pages/chat/MessageBox.js
@@ -8,38 +8,57 @@ import TimePicker from './TimePicker'
 import ConfirmButton from './ConfirmButton'
 import Reminder from './Reminder'
 
+const olaTudoBem = "Olá, tudo bem? Sou o assisistente digital do Biel"
+const vouTeAjudar = "Vou te ajudar a agendar um horário, vamos lá?"
+const informeONumero = "Informe o número do seu WhatsApp, para que possamos entrar em contato com você"
+const qualServico = "Qual serviço você gostaria de agendar?"
+const quandoVoceDeseja = "Quando você deseja realizar esse serviço?"
+const qualOHorario = "Qual o horário que você deseja realizar esse serviço?"
+const seuServico = "Seu Corte de Cabelo então ficou agendado para 09 de Agosto, terça-feira as 08:00"
+const possoConfirmar = "Posso confirmar?"
+const confirmado = "Confirmado então"
+const cliqueNoBotão = "Clique no botão abaixo para enviar um lembrete ao Biel ou feche a página."
+
+
+
 
 export default function MessageBox() {
+
   return (
     <div className='grid grid-cols-1 items-center gap-4 px-4 lg:px-12 pb-[120px]'>
+    {/* PREPARAR AS MESSAGES QUE VAO ENTRAR EM CADA COMPONENTE MESSAGE
+        PREPARAR O CONTEXT QUE OS INPUTS E PICKERS VAO GERENCIAR
+        PREPARAR OS INPUTS, PICKERS E RESPONSES PARA LIDAR COM ESSES VALORES
+        CRIAR OS FECTHS QUE VÃO LIDAR COM O BANCO
+    */}
 
-        <Message />
-        <Message />
-        <Message />
+        <Message message={olaTudoBem}/>
+        <Message message={vouTeAjudar}/>
+        <Message message={informeONumero}/>
         <Input />
         <Response />
 
 
-        <Message />
+        <Message message={qualServico}/>
         <Service />
         <Response />
 
 
-        <Message />
+        <Message message={quandoVoceDeseja}/>
         <Date />
         <Response />
 
-        <Message />
+        <Message message={qualOHorario}/>
         <TimePicker />
         <Response />
 
-        <Message />
-        <Message />
+        <Message message={seuServico}/>
+        <Message message={possoConfirmar}/>
         <ConfirmButton />
 
-        <Message />
-        <Message />
-        <Reminder />
+        <Message message={confirmado}/>
+        <Message message={cliqueNoBotão}/>
+        <Reminder/>
 
     </div>
   )
